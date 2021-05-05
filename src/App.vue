a5ef665edee8797388a05d2fe5f43a9a7c8c48d2
unknown <ppsena@SISTEMAS-006.teltelematica.com.br>
2021-05-05 12:14:24 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+ <Primeiro curso="MEVN" :detalhes="true"/>
+</template>
+
+<script>
+import Primeiro from './components/Primeiro'
+
+export default {
+  name: 'App',
+  components: {
+      Primeiro
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center; 
+}
+</style>
---
+++ src/components/Primeiro.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="topo">
+  <h1>Curso de Inglês</h1>
+  <h3>Desenvolvido por: {{autor}}</h3>
+  <div v-if="detalhes">
+    <h2>Mega Promoção</h2>
+    <h2>De {{preço}} R$ por apenas {{preçoComDesconto}} R$ </h2>
+  </div>  
+ <button>acessar curso de {{curso}}</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: ["curso","detalhes"],
+  data(){
+    return{
+    preço: 499.00,
+    preçoComDesconto: 299.00,
+    autor: 'Italo'
+    }
+  }
+}
+</script>
+
+<style scoped>
+*{
+  margin: 0;
+}
+.topo{
+  padding: 10px;
+  background-color: rgb(199, 53, 82);
+  color: azure;
+}
+h1{
+  margin-bottom: 10px;
+}
+h2{
+  margin-bottom: 5px;
+}
+h3{
+  margin-bottom: 10px;
+}
+button{
+  margin-top: 10px;
+  width: 280px;
+  height: 40px;
+  border: none;
+}
+
+
+</style>
